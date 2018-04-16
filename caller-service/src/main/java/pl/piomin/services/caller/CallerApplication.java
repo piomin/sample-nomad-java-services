4adc65269b3128389d1ba12fd573005492c17663
piotr.minkowski <piotr.minkowski@gmail.com>
2018-04-16 17:07:50 +0200

    adding spring cloud consul support

--- caller-service/src/main/java/pl/piomin/services/caller/CallerApplication.java
+++ caller-service/src/main/java/pl/piomin/services/caller/CallerApplication.java
@@ -2,10 +2,13 @@ package pl.piomin.services.caller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CallerApplication {
 
 	public static void main(String[] args) {
@@ -13,6 +16,7 @@ public class CallerApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
