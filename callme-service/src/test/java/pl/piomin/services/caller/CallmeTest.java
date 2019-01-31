351036b78aa0ff2e05880df7d10de5b5423354e3
minkowp <piotr.minkowski@play.pl>
2019-01-31 15:14:45 +0100

    improvements

--- callme-service/src/test/java/pl/piomin/services/caller/CallmeTest.java
+++ callme-service/src/test/java/pl/piomin/services/caller/CallmeTest.java
@@ -1,9 +1,6 @@
 package pl.piomin.services.caller;
 
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,16 +22,10 @@ public class CallmeTest {
     @Autowired
     TestRestTemplate template;
 
-    static int port;
-    static {
-        PostgreSQLContainer postgresContainer = new PostgreSQLContainer()
-                .withDatabaseName("postgres")
-                .withUsername("postgres")
-                .withPassword("postgres123");
-        postgresContainer.start();
-        port = postgresContainer.getFirstMappedPort();
-        System.setProperty("spring.datasource.url", String.format("jdbc:postgresql://192.168.99.100:%d/postgres", postgresContainer.getFirstMappedPort()));
-    }
+    private static PostgreSQLContainer postgresContainer = new PostgreSQLContainer()
+            .withDatabaseName("postgres")
+            .withUsername("postgres")
+            .withPassword("postgres123");
 
     @ClassRule
     public static VaultContainer vaultContainer = new VaultContainer<>("vault:1.0.2")
@@ -43,12 +34,20 @@ public class CallmeTest {
 
     @BeforeClass
     public static void init() throws IOException, InterruptedException {
+        postgresContainer.start();
+        int port = postgresContainer.getFirstMappedPort();
+        System.setProperty("spring.datasource.url", String.format("jdbc:postgresql://192.168.99.100:%d/postgres", postgresContainer.getFirstMappedPort()));
         vaultContainer.execInContainer("vault", "secrets", "enable", "database");
         String url = String.format("connection_url=postgresql://{{username}}:{{password}}@192.168.99.100:%d?sslmode=disable", port);
         vaultContainer.execInContainer("vault", "write", "database/config/postgres", "plugin_name=postgresql-database-plugin", "allowed_roles=default", url, "username=postgres", "password=postgres123");
         vaultContainer.execInContainer("vault", "write", "database/roles/default", "db_name=postgres",
                 "creation_statements=CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}';GRANT SELECT, UPDATE, INSERT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";GRANT USAGE,  SELECT ON ALL SEQUENCES IN SCHEMA public TO \"{{name}}\";",
                 "default_ttl=1h", "max_ttl=24h");
+    }
+
+    @AfterClass
+    public static void shutdown() {
+        postgresContainer.stop();
     }
 
     @Test
